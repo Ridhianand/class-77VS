e935e261bc2c8bc3b8ce1d72b6ced887a3958c68
Ridhianand <90387640+Ridhianand@users.noreply.github.com>
2022-01-19 15:54:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var name1="Ridhi";
+console.log(name1);
+var last_name="Anand";
+console.log(last_name);
+var full_name= name1.concat(last_name);
+console.log (full_name);
+var name_length= full_name.length;
+console.log(name_length);
+var replace_name= name1.replace("Ridhi","Sharanya");
+console.log(replace_name);
+var capital_word="PARIS";
+var lower_caseword= capital_word.toLowerCase();
+console.log(lower_caseword)
+var lower_word="eagle";
+var upper_caseword= lower_word.toUpperCase();
+console.log(upper_caseword)
+var maximum_number= Math.max(1126,7398);
+console.log(maximum_number)
+var minimum_number= Math.min(6473,9732);
+console.log(minimum_number)
